d6279eef4349953f506bdbebf866ca11bdf755c2
roonil-wazlib <elfhogan@gmail.com>
2020-10-09 00:59:02 +1300

    added documentation

--- final/paddle.c
+++ final/paddle.c
@@ -1,31 +1,32 @@
-//
-// Created by tcr59 on 26/09/20.
-//
+/** @file paddle.c
+ * @author Emma Hogan, Tom Rizzi
+ * @date 26 September 2020
+ * @brief paddle behaviour module
+ * last edited 10 October 2020 by Emma Hogan
+ */
 
 #include "paddle.h"
 
-
-/**
- * Returns the paddles current position
- * @return int of paddle position
- */
+/** Returns the paddles current position:
+    @param paddle pointer to paddle struct
+    @return int of paddle position */
 uint8_t get_paddle_location(Paddle* paddle) {
     return paddle->pos;
 };
 
-/**
- * Initialise paddle
- */
+/** Initialise the paddle struct:
+    @param paddle pointer to paddle struct */
 void paddle_init(Paddle* paddle)
 {
     paddle->pos = PADDLE_START_POS;
 }
 
-/**
- * Returns a bitmap of the current paddle position
- */
+/** Returns a bitmap of the current paddle position:
+    @param paddle pointer to paddle struct 
+    @param bitmap bitmap array for ledmat*/
 void get_paddle_bitmap(Paddle* paddle, uint8_t* bitmap)
 {
+    //TODO GET RID OF THESE MAGIC NUMBERS
     bitmap[4] = 0;
     bitmap[4] |= (1 << (6-paddle->pos));
 
@@ -40,27 +41,8 @@ void get_paddle_bitmap(Paddle* paddle, uint8_t* bitmap)
     }
 }
 
-/**
- * Checks for movement of navswitch, moves paddle accordingly
- */
-void paddle_move_check(Paddle* paddle)
-{
-    navswitch_update();
-    // Check for a left push
-    if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
-        paddle_move_left(paddle);
-    }
-
-    // Check for a right push
-    if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
-        paddle_move_right(paddle);
-    }
-}
-
-/**
- * Move the paddle to the left once.
- * Will only move if the paddle isn't already at the limit
- */
+/** Move the paddle one unit left if possible:
+    @param paddle pointer to paddle struct */
 void paddle_move_left(Paddle* paddle)
 {
     // Check that the paddle isn't at the far limit yet
@@ -69,10 +51,8 @@ void paddle_move_left(Paddle* paddle)
     }
 }
 
-/**
- * Moves the paddle once to the right.
- * Only moves the paddle if the paddle isn't already at the right limit
- */
+/** Move the paddle one unit right if possible:
+    @param paddle pointer to paddle struct */
 void paddle_move_right(Paddle* paddle)
 {
     // Check the paddle isn't at the right limit
--- final/paddle.h
+++ final/paddle.h
@@ -1,6 +1,9 @@
-//
-// Created by tcr59 on 26/09/20.
-//
+/** @file paddle.h
+ * @author Emma Hogan, Tom Rizzi
+ * @date 26 September 2020
+ * @brief paddle behaviour module
+ * last edited 10 October 2020 by Emma Hogan
+ */
 
 #ifndef ASSIGMENT_PADDLE_H
 #define ASSIGMENT_PADDLE_H
@@ -16,11 +19,26 @@ typedef struct {
     uint8_t pos;
 } Paddle;
 
+/** Initialise the paddle struct:
+    @param paddle pointer to paddle struct */
 void paddle_init(Paddle* paddle);
-void paddle_move_check(Paddle* paddle);
+
+/** Move the paddle one unit left if possible:
+    @param paddle pointer to paddle struct */
 void paddle_move_left(Paddle* paddle);
+
+/** Move the paddle one unit right if possible:
+    @param paddle pointer to paddle struct */
 void paddle_move_right(Paddle* paddle);
+
+/** Returns a bitmap of the current paddle position:
+    @param paddle pointer to paddle struct 
+    @param bitmap bitmap array for ledmat*/
 void get_paddle_bitmap(Paddle* paddle, uint8_t* bitmap);
+
+/** Returns the paddles current position:
+    @param paddle pointer to paddle struct
+    @return int of paddle position */
 uint8_t get_paddle_location(Paddle* paddle);
 
 #endif //ASSIGMENT_PADDLE_H
